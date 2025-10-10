0b2f8fc599f491dcff68be43ee733f3378512f02
Nahia Beraza <nahia.beraza06@somo.eus>
2025-10-10 11:56:27 +0200

    Primer commit

---
+++ Ejercicios JavaScript/ejericicos.js
@@ -0,0 +1,14 @@
+function calcularPrecioConIVA(precio, iva) {
+    const cantidadIVA = (precio * iva) / 100;
+    const precioTotal = precio + iva;
+
+    console.log("Precio del producto: " + precio + "€");
+    console.log("IVA (" + iva + "%): " + cantidadIVA + "€");
+    console.log("Precio total a pagar: " + precioTotal + "€");
+
+    return precioTotal;
+}
+
+function areaYperimetroCuadrado(lado){
+    area
+}
